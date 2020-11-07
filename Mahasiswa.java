9c1cdf3c15dfe4282a38ef5b7f58920778107b87
aditmk1 <73748815+aditmk1@users.noreply.github.com>
2020-11-07 19:39:59 +0700

    Add files via upload

---
+++ Mahasiswa.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+    
+
+/**
+ *
+ * @author
+ * NAMA     : Aditiya Musthafa Kamil
+ * KELAS    : IF-2
+ * NIM      : 10119075
+ * Deskripsi Program : Class untuk objek Mahasiswa
+ * 
+ * 
+ */
+public class Mahasiswa {
+    public String nim, nama;
+    
+    public void perkenalkanDiri() {
+        System.out.println("Hallo Everyone");
+        System.out.println("My NIM is ".concat(nim));
+        System.out.println("My Name is ".concat(nama));
+        System.out.println("");
+        
+        System.out.println("===============================================");
+        System.out.println("Developed by Aditiya Musthafa Kamil");
+    }
+}
